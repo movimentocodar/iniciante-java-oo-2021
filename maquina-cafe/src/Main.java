dc1bede423758ee0006b973248606e6d94246d86
Gu1lhermeV <guilhermevictorrocha@gmail.com>
2021-09-09 17:55:30 -0300

    pra salvar

---
+++ maquina-cafe/src/Main.java
@@ -0,0 +1,22 @@
+public class Main extends SistemaInterno {
+		
+	
+	public static void escolhaSabor() {
+		
+		System.out.println("\n  --------- Selecione uma Opção --------- \n");
+		System.out.println("| --> 1. Café .................... R$0.50  |");
+		System.out.println("| --> 2. Café com Leite .......... R$1.00  |");
+		System.out.println("| --> 3. Capuccino ............... R$1.50  |");
+		System.out.println("| --> 4. Chá de Limão ............ R$1.00  |");
+		System.out.println("| --> 5. Água Quente ............. Grátis  |");		
+		System.out.println("\n ------------- 0. Desliga -------------");
+		
+		System.out.println("\n -----> Número do Pedido: ");		
+		
+	}
+	
+	public static void main(String[] args) {				
+		
+	}
+
+}
