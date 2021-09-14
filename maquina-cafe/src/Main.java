1b9edb80419e3eae59481b5257a2d9fed9fb2bea
Gu1lhermeV <guilhermevictorrocha@gmail.com>
2021-09-14 01:14:08 -0300

    Iniciante em java MC

--- maquina-cafe/src/Main.java
+++
@@ -1,9 +0,0 @@
-import abstracts.SistemaInterno;
-import classes.EscolhaPedido;
-
-public class Main extends SistemaInterno {
-	
-	public static void main(String[] args) {				
-		EscolhaPedido escolhaPedido = new EscolhaPedido();	
-	}
-}
--- maquina-cafe/src/exceptions/DinheiroInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-public class DinheiroInsuficienteException extends RuntimeException {
-
-	public DinheiroInsuficienteException() {
-		super("Operação negada. Saldo insuficiente.");
-	}
-	
-}
