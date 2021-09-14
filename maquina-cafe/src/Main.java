1db00cc5ce81413551fb1d196bdf7db87f6d63ba
Gu1lhermeV <guilhermevictorrocha@gmail.com>
2021-09-14 01:25:43 -0300

    Iniciante Java MC

---
+++ maquina-cafe/src/Main.java
@@ -0,0 +1,9 @@
+import abstracts.SistemaInterno;
+import classes.EscolhaPedido;
+
+public class Main extends SistemaInterno {
+	
+	public static void main(String[] args) {				
+		EscolhaPedido escolhaPedido = new EscolhaPedido();	
+	}
+}
