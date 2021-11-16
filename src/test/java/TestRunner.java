766bb1a3678fafb74119242ca78c7da174b962de
Lilian <lilianjaf@gmail.com>
2021-11-16 20:52:33 -0300

    Atualização do Desafio - Test runner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,19 +6,19 @@ import java.util.ArrayList;
 
 public class TestRunner {
     public static void main(String[] args) {
-      ArrayList<Result> resultList = new ArrayList<Result>();
-      resultList.add(JUnitCore.runClasses(MaquinaDeCafeTest.class));
-      resultList.add(JUnitCore.runClasses(CupomTest.class));
-      resultList.add(JUnitCore.runClasses(PagamentoTest.class));
-      resultList.add(JUnitCore.runClasses(ReservatorioDeAguaTest.class));
+        ArrayList<Result> resultList = new ArrayList<Result>();
+        resultList.add(JUnitCore.runClasses(MaquinaDeCafeTest.class));
+        resultList.add(JUnitCore.runClasses(CupomTest.class));
+        resultList.add(JUnitCore.runClasses(PagamentoTest.class));
+        resultList.add(JUnitCore.runClasses(ReservatorioDeAguaTest.class));
 
-      for (Result result: resultList) {
-          for (Failure failure : result.getFailures()) {
-              System.out.println(result.getClass().getName() + " - " + failure.toString());
-          }
+        for (Result result : resultList) {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
 
-          System.out.println(result.getClass().getName() + " - " + result.wasSuccessful());
-      }
-
+            System.out.println(result.wasSuccessful());
+        }
     }
 }
+
