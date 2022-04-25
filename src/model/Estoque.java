83d8efaaf5d64168f6bee811e12921e02814c05f
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-04-24 23:45:55 -0300

    implementado model estoque

--- src/model/Estoque.java
+++ src/model/Estoque.java
@@ -6,4 +6,49 @@ import java.util.Objects;
 
 public abstract class Estoque {
 
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    private static final List<Produto> estoque = new ArrayList<>();
+
+    public static List<Produto> carregarProdutos() {
+        estoque.add(new Produto(1, "Agua", 1000, "ml"));
+        estoque.add(new Produto(2, "Po de Cafe", 1000, "g"));
+        estoque.add(new Produto(3, "Filtro", 10, "un"));
+        estoque.add(new Produto(4, "Leite em Po", 1000, "g"));
+        estoque.add(new Produto(5, "Chocolato em Po", 1000, "g"));
+        estoque.add(new Produto(6, "Cha em po Limao", 1000, "g"));
+        estoque.add(new Produto(7, "Copo", 1000, "un"));
+        estoque.add(new Produto(8, "Acucar", 1000, "g"));
+        return estoque;
+
+    }
+
+    public static double getEstoque(String descricao) {
+
+        return Objects.requireNonNull(carregarProdutos()
+                .stream()
+                .filter(c -> c.getDescricao().equals(descricao)).findAny().orElse(null)).getEstoque();
+    }
+
+    public static List<Produto> getEstoqueProduto() {
+        return estoque;
+    }
+
+    public static void atualizarEstoque(int codigo, int quantidade) {
+        estoque.stream().filter(p -> p.getCodigo() == codigo).findAny().ifPresent(e -> {
+            int qtdEstoque = e.getEstoque() - quantidade;
+            if (e.getCodigo() == codigo) {
+                e.setEstoque(qtdEstoque);
+            }
+        });
+    }
+
 }
