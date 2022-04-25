3dd615041e1dac91d66936d63df52ea64677f1a4
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-04-24 23:49:22 -0300

    implementado model pedido

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -5,4 +5,43 @@ import java.util.List;
 
 public class Pedido{
 
+    private final int quantidade;
+    private final Produto produto;
+    private  static  final List<Pedido> listaPedido = new ArrayList<>();
+
+    public Pedido(Produto produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "quantidade=" + quantidade +
+                ", produto=" + produto +
+                '}';
+    }
+
+    public static void addPedido(Produto produto, int quantidade) {
+        Pedido pedido = new Pedido (produto, quantidade);
+        listaPedido.add(pedido);
+    }
+
+    public static List<Pedido> getListaDePedidos() {
+        return listaPedido;
+    }
+
+    public static void resetListaPedida(){
+        listaPedido.clear();
+    }
+
 }
