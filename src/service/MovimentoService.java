c8173430fe58f4b93fd8dfb8f580690fff0a7bba
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-04-24 23:55:13 -0300

    implementado movimento service

--- src/service/MovimentoService.java
+++ src/service/MovimentoService.java
@@ -7,4 +7,10 @@ import java.util.List;
 
 public class MovimentoService {
 
+    public  void atualizarEstoque(List<Pedido> pedidos, int quantidadeDeAcucar){
+
+        pedidos.forEach(p -> Estoque.atualizarEstoque(p.getProduto().getCodigo(), p.getQuantidade()));
+
+        Estoque.atualizarEstoque(8, quantidadeDeAcucar);
+    }
 }
