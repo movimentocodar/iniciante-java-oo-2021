3bc6456a6f76664a9e54cc1156e0f621cfb66094
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-04-24 23:44:03 -0300

    adicionado menu e painel

--- src/menu/MenuAbastecer.java
+++ src/menu/MenuAbastecer.java
@@ -3,6 +3,11 @@ package menu;
 import controller.ControlerReabastecer;
 
 public class MenuAbastecer {
+    private static final ControlerReabastecer controlerReabastecer = new ControlerReabastecer();
+
+    public static void abastecer(){
+        controlerReabastecer.consultarEstoque();
+    }
 
 
 }
