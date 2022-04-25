1c38dda9d6dd5c934c3f20206936c982effb3491
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-04-24 23:46:39 -0300

    implementado model ingrediente

--- src/model/Ingrediente.java
+++ src/model/Ingrediente.java
@@ -2,5 +2,27 @@ package model;
 
 public class Ingrediente {
 
+    private final int codigo;
+    private final int quantidade;
 
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public Ingrediente(int codigo, int quantidade) {
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingrediente{" +
+                "codigo=" + codigo +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
