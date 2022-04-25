5b7ceffcf2ae69904b61fe57b1b3985046b4f9ef
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-04-24 23:50:24 -0300

    implementado model produto

--- src/model/Produto.java
+++ src/model/Produto.java
@@ -4,4 +4,63 @@ import java.util.Objects;
 
 public class Produto {
 
+    private final int codigo;
+    private final String descricao;
+    private int estoque;
+    private final String embalagem;
+
+
+    public Produto(int codigo, String descricao, int estoque, String embalagem) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.estoque = estoque;
+        this.embalagem = embalagem;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Produto produto = (Produto) o;
+        return codigo == produto.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", descricao='" + descricao + '\'' +
+                ", estoque=" + estoque +
+                '}';
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
 }
