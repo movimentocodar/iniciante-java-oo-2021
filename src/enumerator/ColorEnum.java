df7f5107f3843a612c91964bfb20e90692a93998
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-05-11 20:18:08 -0300

    renomeado a class para um nome mais intuitivo

--- src/enumerator/ColorEnum.java
+++ src/enumerator/ColorEnum.java
@@ -1,11 +1,11 @@
-package model;
+package enumerator;
 
-public enum Color {
+public enum ColorEnum {
    YELLOW("\u001B[33m"), RED("\u001B[31m"), GREEN("\u001B[32m"), BLUE("\u001B[34m"), RESET("\u001B[0m");
 
    final String colorCode;
 
-   Color(String colorCode) {
+   ColorEnum(String colorCode) {
       this.colorCode = colorCode;
    }
 
