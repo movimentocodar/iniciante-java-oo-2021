2e1fc1a16cd5e21a2f3696a74be866836717aabd
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-05-11 20:36:53 -0300

    removido a consulta de saldo de estoque

--- src/service/AbastecimentoService.java
+++ src/service/AbastecimentoService.java
@@ -1,70 +1,39 @@
 package service;
 
-import menu.Painel;
-import model.Color;
-import util.InputScanner;
 
+import enumerator.ColorEnum;
+import model.Produto;
+
+import java.util.List;
+import java.util.Map;
+
+import static mensagem.MensagemMaquinaAbastecida.maquinaAbastecida;
 
 public class AbastecimentoService {
 
-    private  final int ESTOQUEPADRAO = 1000;
-    private  int necessidade;
-    private static final InputScanner input = new InputScanner();
-    private final EstoqueService estoqueService = new EstoqueService();
+    public void abastecer(Map<Produto, List<Double>> produtoEQuantidade) {
+        boolean abastecida = true;
+        for (Map.Entry<Produto, List<Double>> produto : produtoEQuantidade.entrySet()){
+            if (produto.getKey().getQuantidade() < 1000){
+                double quantidadeAnterior = produto.getKey().getQuantidade();
+                int ESTOQUEPADRAO = 1000;
+                produto.getKey().setQuantidade(ESTOQUEPADRAO);
+                produtoReabastecido(produto, quantidadeAnterior);
+                abastecida = false;
+            }
 
+        }
+        maquinaAbastecida(abastecida);
 
-    public int consultarSaldoDeEstoquePorProduto() {
-
-        estoqueService.getEstoque().stream().filter(e -> e.getEstoque() < 1000).forEach(produto -> {
-            necessidade = Math.abs(produto.getEstoque() - 1000);
-            System.out.println(Color.RED.getColorCode()
-                    + "\nQuantidade de " + produto.getDescricao()
-                    + " insuficiente, a maquina precisa ser reabastecida!"
-                    + "\nQuantidade faltante: "
-                    + necessidade
-                    + produto.getEmbalagem() + "\n"
-                    + Color.RESET.getColorCode());
-        });
-        return necessidade;
     }
 
-    public void reabastecerAMaquina() {
-        int opcao;
-
-        do {
-            opcao = getReabastecimento("Deseja reabastecer a maquina?\n");
-
-        } while (opcao < 1 || opcao > 2);
-
-        do {
-            opcao = getReabastecimento("Confirmar o reabastecimento?\n");
-
-        } while (opcao < 1 || opcao > 2);
-
-
-        estoqueService.getEstoque().stream().filter(e -> e.getEstoque() < 1000).forEach(
-                produto -> produto.setEstoque(ESTOQUEPADRAO));
-
-
-        System.out.println(Color.GREEN.getColorCode() + "\nMaquina reabastecida com sucesso!" + Color.RESET.getColorCode());
-        necessidade = 0;
+    private void produtoReabastecido(Map.Entry<Produto, List<Double>> produtoEQuantidade, double quantidadeAnterior){
+        System.out.println(ColorEnum.GREEN.getColorCode() + "*** Item reabastecido  *** " + ColorEnum.RESET.getColorCode()
+                + "\n" + produtoEQuantidade.getKey().getDescricao()
+                + "\n Quantidade atualizada: de "
+                + ColorEnum.RED.getColorCode() +  quantidadeAnterior
+                + ColorEnum.RESET.getColorCode() + " para " + ColorEnum.GREEN.getColorCode() + produtoEQuantidade.getKey().getQuantidadeMaxima()
+                + ColorEnum.RESET.getColorCode() + "\n"
+        );
     }
-
-
-
-    private  int getReabastecimento(String x) {
-        System.out.print(Color.YELLOW.getColorCode() + x
-                + Color.RESET.getColorCode()
-                + "\n(1) Sim"
-                + "\n(2) Nao\n");
-        System.out.print(Color.YELLOW.getColorCode() + "\nOpcao n: " + Color.RESET.getColorCode());
-        int reabastecimento = input.entrada();
-
-        System.out.println((reabastecimento < 1 || reabastecimento > 2) ? Color.RED.getColorCode() + "\nOpcao invalida!\n" + Color.RESET.getColorCode() : "");
-        if (reabastecimento == 2) Painel.menuPrincipal();
-        return reabastecimento;
-    }
-
-
-
 }
