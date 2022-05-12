3c9b7e6aa347e9a5796271220ad91698fb9ee9e3
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-05-11 21:22:08 -0300

    classe para calcular a quantidade de bebida a ser produzida

---
+++ src/calculadora/CalcularQuantidadeAProduzir.java
@@ -0,0 +1,45 @@
+package calculadora;
+
+import menu.fabrica.Bebida;
+import model.Estoque;
+import model.Ingrediente;
+import model.Produto;
+
+import java.util.*;
+
+public class CalcularQuantidadeAProduzir {
+    
+    private static final int CODIGOKEYACUCAR = 8;
+
+    private static final Map<Produto, List<Double>> produtosEQuantidade = new HashMap<>();
+    private final Estoque estoque = new Estoque();
+
+    public void produto(Bebida bebida, int qtdCopos, int nivelDeAcucar) {
+        produtosEQuantidade.clear();
+
+        List<Ingrediente> ingredientes =  bebida.getIngredients();
+        for (Ingrediente ingrediente : ingredientes) {
+
+            estoque.getEstoqueListaDeProduto().stream().filter(p -> p.getProdutoKey() == ingrediente.getProdutoKey()).findAny()
+                    .ifPresent(produto -> calcular(produto, qtdCopos, ingrediente.getQuantidade(), nivelDeAcucar));
+        }
+    }
+
+    private void calcular(Produto produto, int qtdCopos, double ingredienteQuantidade, int nivelDeAcucar) {
+        List<Double> quantidadeTotal = new ArrayList<>();
+
+        if (produto.getProdutoKey() == CODIGOKEYACUCAR) {
+            quantidadeTotal.add(qtdCopos * (ingredienteQuantidade * nivelDeAcucar));
+            quantidadeTotal.add(ingredienteQuantidade);
+
+        } else {
+            quantidadeTotal.add(qtdCopos * ingredienteQuantidade);
+            quantidadeTotal.add(ingredienteQuantidade);
+        }
+        produtosEQuantidade.put(produto, quantidadeTotal);
+    }
+
+    public Map<Produto, List<Double>> getProdutosEQuantidade() {
+        return Collections.unmodifiableMap(produtosEQuantidade);
+    }
+}
