f89d21931de9b4f9779e9793c4bcd4895adb0819
Hugo Ferreira <34543466+hugoogle@users.noreply.github.com>
2022-05-24 00:25:26 -0300

    removido import

--- src/main/java/br/com/hugoogle/menu/fabrica/FabricaMenuReabastecer.java
+++ src/main/java/br/com/hugoogle/menu/fabrica/FabricaMenuReabastecer.java
@@ -3,7 +3,6 @@ package br.com.hugoogle.menu.fabrica;
 import br.com.hugoogle.calculadora.CalcularQuantidadeAProduzir;
 import br.com.hugoogle.controller.AbastecimentoController;
 import br.com.hugoogle.menu.submenu.SubMenuCompra;
-import br.com.hugoogle.service.AbastecimentoService;
 
 public class FabricaMenuReabastecer {
     private static final int SIM = 1;
